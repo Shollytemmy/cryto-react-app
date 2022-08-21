b77072a800f311d4e13405afff97fb730ce5ef0d
Bunyameen <bunyameen@gmail.com>
2022-08-21 16:14:34 +0100

    added another folder keep/keep.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
   const fetchCoin = async() =>{
     const base_Url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
-    const response = await fetch(base_Url)
-    const responseJson = await response.json()
-    console.log(responseJson)
+   
 
   }
 
---
+++ src/keep/keep.js
@@ -0,0 +1,3 @@
+ const response = await fetch(base_Url)
+    const responseJson = await response.json()
+    console.log(responseJson)
